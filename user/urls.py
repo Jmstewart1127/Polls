dda5e11c91c7c3081196bdba7846f11c430ad243
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-03-22 17:46:22 -0500

    user api

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewSet)
 
 app_name = 'user'
 urlpatterns = [
@@ -8,4 +13,8 @@ urlpatterns = [
     path('user/added', views.create_user, name='addUser'),
     path('login/', views.login_page, name='login'),
     path('welcome/', views.user_login, name='welcome'),
+
+    ## REST URLS ##
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,13 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import viewsets
+from user.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 def create_user_page(request):
